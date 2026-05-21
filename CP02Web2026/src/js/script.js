d698f130e9b100206dd25f5f3eded01039ecd40f
Kelso <koasobrinho2008@gmail.com>
2026-05-21 17:08:34 -0300

    fix: corrige erro de loop infinito e erros de digitação

--- CP02Web2026/src/js/script.js
+++ CP02Web2026/src/js/script.js
@@ -4,19 +4,19 @@ function cadastrarVinho(){
     while(confirm("Você deseja cadastrar um vinho ?")){
         let nomeVinho = prompt("Informe o nome do vinho: ");
         while (nomeVinho == null){
-            prompt("Informe o nome do vinho: ");
+           nomeVinho = prompt("Informe o nome do vinho: ");
         }
         let novoEstoqueVinho = parseInt(prompt("Informe a quantidade de vinho a ser armazenada: "));
         while(novoEstoqueVinho == null){
-            parseInt(prompt("Informe a quantidade de vinho a ser armazenada: "));
+            novoEstoqueVinho = parseInt(prompt("Informe a quantidade de vinho a ser armazenada: "));
         }
         let safraVinho = parseInt(prompt("Indique qual a safra do vinho: "));
         while (safraVinho == null){
-            parseInt(prompt("Indique qual a safra do vinho: "));
+            safraVinho = parseInt(prompt("Indique qual a safra do vinho: "));
         }
         let tipoVinho = prompt("Indique qual o tipo do vinho: ");
         while (tipoVinho == null){
-            prompt("Indique qual o tipo do vinho: ");
+            tipoVinho = prompt("Indique qual o tipo do vinho: ");
         }
         estoqueVinho[nomeVinho] = (estoqueVinho[nomeVinho] ?? 0) + novoEstoqueVinho;
         alert(`${nomeVinho} foi adicionado e agora existem ${estoqueVinho[nomeVinho]} desse vinho !`);
@@ -26,13 +26,13 @@ function cadastrarVinho(){
 function listarVinhos() {
   for (const nome in estoqueVinho) {
     const v = estoqueVinho[nome];
-    alert(`${nome} | Tipo: ${v.tipo} | Safra: ${v.safra} | Quantidade: ${v.quantidade}`);
+    alert(`${nome} | Tipo: ${v.tipoVinho} | Safra: ${v.safraVinho} | Quantidade: ${v.novoEstoqueVinho}`);
   }
 }
 function verificarEstoque(){
     const nomeVinho = prompt("Informe o nome do vinho que deseja verificar: ");
     if (estoqueVinho[nomeVinho] < 5){
-        alert(`O estoque está baixo, está em ${estoqueVinho[nomeVinho]}`);
+        alert(`O estoque está baixo, possuem ${estoqueVinho[nomeVinho]} unidades em estoque`);
     }
     else{
         alert(`O estoque do vinho ${nomeVinho} é de ${estoqueVinho[nomeVinho]}`);
